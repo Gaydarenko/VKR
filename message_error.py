69da011d8dfbc28ee5ee81bb0846328a242e9c8d
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2021-02-19 14:17:27 +0300

    Вывод окна ошибки с переданным текстом проблемы и прекращение выполнение программы.

---
+++ message_error.py
@@ -0,0 +1,23 @@
+"""
+Всё для вывода сообщений с ошибками
+"""
+import tkinter as tk
+from sys import exit as sys_exit
+
+
+class MessageError:
+
+    def __init__(self, text: str):
+        self.text = text
+
+    def message_window(self):
+        """
+        Вывод окна ошибки с переданным текстом проблемы и прекратит выполнение программы.
+        :return:
+        """
+        window = tk.Tk()
+        window.title("Ошибка!!!")
+        label = tk.Label(f"{self.text}", height=2)
+        label.pack()
+        window.mainloop()
+        sys_exit()
