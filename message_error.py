cf48a15751eda8842cec1dc0860d9a78a4951352
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2021-02-27 17:56:05 +0300

    Метод преобразован в статический

--- message_error.py
+++ message_error.py
@@ -7,17 +7,15 @@ from sys import exit as sys_exit
 
 class MessageError:
 
-    def __init__(self, text: str):
-        self.text = text
-
-    def message_window(self):
+    @staticmethod
+    def message_window(text: str) -> None:
         """
         Вывод окна ошибки с переданным текстом проблемы и прекратит выполнение программы.
-        :return:
+        :return: None
         """
         window = tk.Tk()
         window.title("Ошибка!!!")
-        label = tk.Label(text=f"{self.text}", height=2, width=40)
+        label = tk.Label(text=f"{text}", height=2, width=40)
         label.pack()
         window.mainloop()
         sys_exit()
